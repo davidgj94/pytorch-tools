f9cd2cfb36f304b2f388b87144988ca995fbe9a2
David <davidgj1994@gmail.com>
2020-01-25 09:45:58 +0100

    entrenamiento ha funcionado

--- train_val.py
+++ train_val.py
@@ -42,25 +42,31 @@ def parse_args():
 	parser = ArgumentParser()
 	parser.add_argument('--config', type=str, required=True, nargs='+')
 	parser.add_argument('--num_epochs', type=int, required=True, nargs='+')
+	parser.add_argument('--parts', type=int, required=True, nargs='+')
 	parser.add_argument('-cpu', dest='use_cpu', action='store_true')
 	parser.set_defaults(use_cpu=False)
 	return parser.parse_args()
 
-def main(config, num_epochs, use_cpu):
+def main(config, num_epochs, use_cpu, part):
+
+	id_list_path = os.path.join('list', 'partition_{}'.format(part), 'train.txt')
+	exper_name = os.path.basename(config).split(".")[0]
+	root_checkpoint_dir = os.path.join('checkpoint', 'partition_{}'.format(part))
+	checkpoint_dir = os.path.join(root_checkpoint_dir, exper_name)
 
 	num_classes, training_cfg, val_cfg = utils.get_cfgs(config)
 	device = torch.device("cuda:0" if torch.cuda.is_available() and not use_cpu else "cpu")
 
-	id_list_path = os.path.join('test', 'list', 'train.txt')
 	model_train = get_model(num_classes, training_cfg["model"]).to(device)
 	train_dataloader = get_dataloader(id_list_path, training_cfg['dataset'], training_cfg['batch_size'], shuffle=True)
 	criterion = get_loss(training_cfg['loss'])
 
-	exper_name = os.path.basename(config).split(".")[0]
-	checkpoint_dir = os.path.join('test', 'checkpoint', exper_name)
-	last_checkpoint_path = get_last_checkpoint(checkpoint_dir)
 	current_epoch = 0
 	learning_rate = training_cfg.get("learning_rate", 0.0005)
+
+	last_checkpoint_path = get_last_checkpoint(checkpoint_dir)
+	if (last_checkpoint_path is None) and ("init" in training_cfg):
+		last_checkpoint_path = os.path.join(root_checkpoint_dir, training_cfg["init"])
 
 	if last_checkpoint_path is not None:
 		last_checkpoint = torch.load(last_checkpoint_path)
@@ -69,7 +75,6 @@ def main(config, num_epochs, use_cpu):
 		learning_rate = last_checkpoint.get("learning_rate", learning_rate)
 		print("CHECKPOINT: {}".format(last_checkpoint_path))
 		print("Learning rate: {}".format(learning_rate))
-
 
 	optimizer = optim.SGD(model_train.trainable_parameters(True), 
 						lr=learning_rate, 
@@ -99,5 +104,6 @@ def main(config, num_epochs, use_cpu):
 
 if __name__ == "__main__":
 	args = parse_args()
-	for config_path, num_epochs in zip(args.config, args.num_epochs):
-		main(config_path, num_epochs, args.use_cpu)
+	for part in args.parts:
+		for config_path, num_epochs in zip(args.config, args.num_epochs):
+			main(config_path, num_epochs, args.use_cpu, part)
