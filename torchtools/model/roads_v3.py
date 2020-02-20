deaaea586f899f41216b642ad8aff4807f6df704
David <davidgj1994@gmail.com>
2020-02-20 16:30:54 +0100

    added roads_v3

--- torchtools/model/roads_v3.py
+++ torchtools/model/roads_v3.py
@@ -52,6 +52,8 @@ def padding(dilation, kernel_size):
 
 def create_convnet(conv_layer, planes, kernel_size, padding, dilation, groups, **kwargs):
 
+	pdb.set_trace() # Ver el contenido de kwargs
+
 	def _init_conv(m):
 		if isinstance(m, OrientedConv2d):
 			m.reset_parameters()
@@ -83,46 +85,114 @@ def forward_ori(ori_net, x, idx):
 	return ori_net(x)
 
 
-class Deeplabv3_Roads(Deeplabv3Plus):
-	def __init__(self, n_classes, pretrained_model, freeze_backbone=False):
+class Deeplabv3_Roads(Deeplabv3):
+	def __init__(self, n_classes, pretrained_model, freeze_backbone=False, freeze_aspp=False):
 		super(Deeplabv3_Roads, self).__init__(n_classes, pretrained_model, aux=False)
-		self.freeze_parameters(freeze_backbone)
+		self.freeze_parameters(freeze_backbone, freeze_aspp)
 		self.params_group = super(Deeplabv3_Roads, self).trainable_parameters(True)
 	
-	def freeze_parameters(self, freeze_backbone):
+	def freeze_parameters(self, freeze_backbone, freeze_aspp):
 		for name, param in self.named_parameters():
 			if ('backbone' in name) and freeze_backbone:
+				param.requires_grad = False
+			elif ('aspp' in name) and freeze_aspp:
 				param.requires_grad = False
 	
 	def trainable_parameters(self):
 		return self.params_group
 
-@register.attach('roads_net_stage1')
-class RoadsNet_Stage1(Deeplabv3_Roads):
+@register.attach('roads_net_multitask')
+class RoadsNetMultitask(Deeplabv3_Roads):
 	def __init__(self, n_classes, pretrained_model,
-					min_angle=0.0, max_angle=180.0, angle_step=15.0, 
-					grid_size=7, dilation=1, ori_planes=[256, 128, 64, 32],
-					norm_groups=8, freeze_backbone=False):
+					min_angle=0.0, max_angle=180.0, angle_step=15.0,
+					layer1_channels=64, layer2_channels=64, 
+					decoder_1_grid_size=7, decoder_1_planes=[128, 64],
+					decoder_2_kernel_size=3, decoder_2_planes=[128, 128], norm_groups=8,
+					stage2=False, fuse_planes=[256, 256, 256], fuse_kernel_size=3, fuse_dilation=2,
+					freeze_backbone=False, freeze_aspp=False, 
+					freeze_decoder_1=False, freeze_decoder_2=False,):
 
-		super(RoadsNet_Stage1, self).__init__(n_classes, pretrained_model, freeze_backbone=freeze_backbone)
+		super(RoadsNetMultitask, self).__init__(n_classes, pretrained_model, 
+													freeze_backbone=freeze_backbone,
+													freeze_aspp=freeze_aspp)
 
 		self.rot_angles = np.arange(min_angle, max_angle + angle_step, angle_step)
 		self.n_angles = len(self.rot_angles)
 
-		padding_ori = padding(dilation, grid_size)
-		kernel_size_ori = grid_size + 8
+		# Decoder 1
+
+		self.reduce_layer1 = nn.Sequential(
+			nn.Conv2d(256, layer1_channels, kernel_size=1, stride=1, bias=False),
+			nn.GroupNorm(norm_groups, layer1_channels),
+			nn.ReLU(),
+			nn.Dropout(0.5))
+		self.reduce_layer1.apply(init_conv)
+
+		decoder_1_planes = [256+layer1_channels] + decoder_1_planes
+		kernel_size_ori = decoder_1_grid_size + 8
 		self.ori_net = create_convnet(OrientedConv2d, 
-										ori_planes, 
+										decoder_1_planes, 
 										kernel_size_ori, 
-										padding_ori, 
-										dilation, 
-										norm_groups,
+										padding(1, decoder_1_grid_size), 
+										dilation=1, 
+										groups=norm_groups,
 										min_angle=min_angle,
 										max_angle=max_angle,
 										angle_step=angle_step)
 
-		self.aux_ori_clf = nn.Conv2d(ori_planes[-1], 1, kernel_size=1, stride=1, bias=False)
-		init_conv(self.aux_ori_clf)
+		self.decoder_1_clf = nn.Conv2d(decoder_1_planes[-1], 1, kernel_size=1, stride=1, bias=False)
+		init_conv(self.decoder_1_clf)
+
+		# Decoder 2
+
+		self.reduce_layer2 = nn.Sequential(
+			nn.Conv2d(256, layer2_channels, kernel_size=1, stride=1, bias=False),
+			nn.GroupNorm(norm_groups, layer2_channels),
+			nn.ReLU(),
+			nn.Dropout(0.5))
+		self.reduce_layer2.apply(init_conv)
+
+		decoder_2_planes = [256+layer2_channels] + decoder_2_planes
+		self.decoder_2_net = create_convnet(nn.Conv2d, 
+										decoder_2_planes, 
+										decoder_2_kernel_size, 
+										padding(1, decoder_2_kernel_size), 
+										dilation=1, 
+										groups=norm_groups,)
+
+		self.decoder_2_clf = nn.Conv2d(decoder_2_planes[-1], 1, kernel_size=1, stride=1, bias=False)
+		init_conv(self.decoder_2_clf)
+
+		self.reduce_decoder_1 = None
+		self.fuse_net = None
+		self.roads_clf = None
+
+		if stage2:
+
+			decoder_1_out = (self.n_angles - 1) * decoder_1_planes[-1]
+			self.reduce_decoder_1 = nn.Sequential(
+										nn.Conv2d(decoder_1_out, 256, kernel_size=1, stride=1, bias=False),
+										nn.GroupNorm(norm_groups, 256),
+										nn.ReLU(),
+										nn.Dropout(0.5))
+			self.reduce_decoder_1.apply(init_conv)
+
+			self.fuse_net = create_convnet(nn.Conv2d, 
+										fuse_planes, 
+										fuse_kernel_size, 
+										padding(fuse_dilation, fuse_kernel_size), 
+										dilation=fuse_dilation, 
+										groups=norm_groups,)
+
+			in_channels_roads = fuse_planes[-1] + decoder_2_planes[-1]
+			self.roads_clf = nn.Sequential(
+									nn.Conv2d(in_channels_roads, 256, kernel_size=1, stride=1, bias=False),
+									nn.GroupNorm(norm_groups, 256),
+									nn.ReLU(),
+									nn.Dropout(0.1,),
+									nn.Conv2d(256, 1, kernel_size=1, stride=1, bias=False))
+			self.roads_clf.apply(init_conv)
+
 
 
 	def _forward_dir(self, x, idx):
@@ -130,7 +200,7 @@ class RoadsNet_Stage1(Deeplabv3_Roads):
 
 
 	def eval(self,):
-		super(RoadsNet_Stage1, self).eval()
+		super(RoadsNetMultitask, self).eval()
 		for module in self.ori_net._modules.values():
 			if isinstance(module, OrientedConv2d):
 				module.eval()
@@ -138,181 +208,62 @@ class RoadsNet_Stage1(Deeplabv3_Roads):
 
 	def trainable_parameters(self, base_lr, alfa=10,):
 
-		params_groups_2 = list(super(RoadsNet_Stage1, self).trainable_parameters())
+		params_groups_2 = list(super(RoadsNetMultitask, self).trainable_parameters())
 
-		params_groups_1 = []
-		params_groups_1 += list(self.ori_net.parameters())
-		params_groups_1 += list(self.aux_ori_clf.parameters())
+		decoder_1_params = []
+		decoder_1_params += list(self.reduce_layer1.parameters())
+		decoder_1_params += list(self.ori_net.parameters())
+		decoder_1_params += list(self.decoder_1_clf.parameters())
 
-		total_params_groups = [{'params': iter(params_groups_1), 'lr': base_lr}]
+		decoder_2_params = []
+		decoder_2_params += list(self.reduce_layer2.parameters())
+		decoder_2_params += list(self.decoder_2_net.parameters())
+		decoder_2_params += list(self.decoder_2_clf.parameters())
+
+		total_params_groups = [{'params': iter(decoder_1_params+decoder_2_params), 'lr': base_lr}]
 		if len(params_groups_2) > 0:
 			total_params_groups += [{'params': iter(params_groups_2), 'lr': base_lr / alfa}]
 		return total_params_groups
 
 
+	def forward_decoder1(self, x_aspp, x_layer1):
+
+		layer1_size = x_layer1.shape[-2:]
+		x_layer1 = self.reduce_layer1(x_layer1)
+		x_aspp_up = F.interpolate(x_aspp, size=layer1_size, mode='bilinear', align_corners=False)
+		x_concat = torch.cat([x_layer1, x_aspp_up], dim=1)
+
+		x_ori = []
+		for idx in np.arange(self.n_angles-1):
+			x_ori.append(self._forward_dir(x_concat, idx))
+
+		return torch.cat(x_ori, dim=0)
+
+	
+	def forward_decoder2(self, x_aspp, x_layer2):
+		x_layer2 = self.reduce_layer2(x_layer2)
+		x_concat = torch.cat([x_layer2, x_aspp], dim=1)
+		return self.decoder_2_net(x_concat)
+
+
 	def forward(self, inputs, return_intermediate=False):
 
 		input_shape = inputs["image"].shape[-2:]
-		features = super(RoadsNet_Stage1, self).forward(inputs, return_intermediate=True)
-		x_decoder = features["decoder"]
+		features = super(RoadsNetMultitask, self).forward(inputs, return_intermediate=True)
+		pdb.set_trace()
+		x_aspp = features["aspp"]
+		x_out1 = self.forward_decoder1(x_aspp, features["layer1"])
+		x_out2 = self.forward_decoder2(x_aspp, features["layer2"])
 
-		# Se asume batch size de 1
-		x_ori = []
-		for idx in np.arange(self.n_angles-1):
-			x_ori.append(self._forward_dir(x_decoder, idx))
-		
-		seg_ori = compute_seg(torch.cat(x_ori, dim=0), input_shape, self.aux_ori_clf).squeeze(1).unsqueeze(0)
-
-		if return_intermediate:
-			features['ori'] = x_ori
-			features['seg_ori'] = seg_ori
-			return features
+		seg_1 = compute_seg(x_out1, input_shape, self.decoder_1_clf)
+		seg_2 = compute_seg(x_out2, input_shape, self.decoder_2_clf)
 
 		result = OrderedDict()
 		if self.training:
-			result["seg_ori"] = seg_ori
-		else:
-			result["seg"] = (seg_ori > 0).cpu()
+			result["seg_1"] = seg_1
+			result["seg_2"] = seg_2
 
 		return result
-
-
-@register.attach('roads_net_stage2')
-class RoadsNet_Stage2(RoadsNet_Stage1):
-	def __init__(self, n_classes, pretrained_model,
-					min_angle=0.0, max_angle=180.0, angle_step=30.0, 
-					grid_size=7, dilation=1, ori_planes=[256, 128, 64, 32],
-					fuse_kernel_size=5, fuse_dilation=2, fuse_planes=[192, 128, 128],
-					norm_groups=8, freeze_backbone=True,):
-		super(RoadsNet_Stage2, self).__init__(n_classes, pretrained_model,
-													min_angle=min_angle, max_angle=max_angle, angle_step=angle_step,
-													grid_size=grid_size, dilation=dilation, ori_planes=ori_planes,
-													norm_groups=norm_groups, freeze_backbone=freeze_backbone)
-
-		padding_fuse = padding(fuse_dilation, fuse_kernel_size)
-		self.fuse_net = create_convnet(nn.Conv2d, 
-										fuse_planes, 
-										fuse_kernel_size, 
-										padding_fuse, 
-										fuse_dilation, 
-										norm_groups,)
-
-		decoder_channels = fuse_planes[-1] // 4
-		self.reduce_decoder = nn.Sequential(
-			nn.Conv2d(256, decoder_channels, kernel_size=1, stride=1, bias=False),
-			nn.GroupNorm(norm_groups, decoder_channels),
-			nn.ReLU(),
-			nn.Dropout(0.5))
-		self.reduce_decoder.apply(init_conv)
-
-		self.roads_clf = nn.Sequential(
-			nn.Conv2d(decoder_channels + fuse_planes[-1], fuse_planes[-1], kernel_size=1, stride=1, bias=False),
-			nn.GroupNorm(norm_groups, fuse_planes[-1]),
-			nn.ReLU(),
-			nn.Dropout(0.1),
-			nn.Conv2d(fuse_planes[-1], 1, kernel_size=1, stride=1, bias=False),)
-		self.roads_clf.apply(init_conv)
-	
-	def trainable_parameters(self, base_lr, alfa):
-		param_groups = super(RoadsNet_Stage2, self).trainable_parameters(base_lr, alfa=alfa)
-		param_groups_1 = list(param_groups[0]['params'])
-		param_groups_1 += list(self.fuse_net.parameters())
-		param_groups_1 += list(self.reduce_decoder.parameters())
-		param_groups_1 += list(self.roads_clf.parameters())
-		param_groups[0] = dict(params=iter(param_groups_1), lr=base_lr)
-		return param_groups
-	
-	def forward(self, inputs):
-
-		input_shape = inputs["image"].shape[-2:]
-
-		features = super(RoadsNet_Stage2, self).forward(inputs, return_intermediate=True)
-		x_decoder = features['decoder']
-		x_ori = features['ori']
-		seg_ori = features['seg_ori']
-
-		x_fuse = self.fuse_net(torch.cat(x_ori, dim=1))
-		x_decoder = self.reduce_decoder(x_decoder)
-		x_concat = torch.cat([x_fuse, x_decoder], dim=1)
-		seg_roads = compute_seg(x_concat, input_shape, self.roads_clf)
-
-		result = OrderedDict()
-		if self.training:
-			result['seg_ori'] = seg_ori
-			result['seg_roads'] = seg_roads
-		
-		return result
-
-
-@register.attach('test_roads_v2')
-class OrientedNetTest(RoadsNet_Stage1):
-	def __init__(self, n_classes, pretrained_model, aux=False, angle_step=15.0):
-
-		grid_size = 25
-		kernel_size = grid_size + 8
-		ori_planes=[256, 1]
-
-		super(OrientedNetTest, self).__init__(n_classes, 
-											  pretrained_model, 
-											  grid_size=grid_size,
-											  ori_planes=ori_planes)
-
-		test_weight = torch.FloatTensor(gabor(np.pi/2, kernel_size))
-		test_weight = test_weight.unsqueeze(0).repeat(ori_planes[0],1,1)
-		test_weight = test_weight.unsqueeze(0).repeat(ori_planes[1],1,1,1)
-		for module in self.ori_net._modules.values():
-			if isinstance(module, OrientedConv2d):
-				module.weight = nn.Parameter(test_weight, requires_grad=False)
-		
-		self.save_dir = os.path.join('pruebas', 'masks')
-		if os.path.exists(self.save_dir):
-			shutil.rmtree(self.save_dir, ignore_errors=True)
-		os.makedirs('masks')
-
-	def get_weight(self, idx):
-		for module in self.ori_net._modules.values():
-			if isinstance(module, OrientedConv2d):
-				module.set_grid(idx)
-				return module.rotate_weight()
-		return None
-
-
-	def forward(self, inputs):
-
-		image_id = inputs["image_id"][0]
-		save_dir = os.path.join(self.save_dir, image_id)
-		os.makedirs(save_dir)
-		label = inputs["label"].squeeze()
-		cv2.imwrite(os.path.join(save_dir, 'label.png'), (label.numpy() * 255).astype(np.uint8))
-		# plt.figure()
-		# plt.imshow(label)
-		weights = inputs['ori_weights'].squeeze()
-		max_idx = [idx for idx, _weight_mask in enumerate(weights) if not np.all(_weight_mask.numpy() < 1.0)].pop()
-		weights = weights[max_idx].numpy()
-		ori_gt = inputs['ori_gt'].squeeze()[max_idx].numpy()
-
-		vis_all_ori = (inputs['ori_gt'].squeeze().sum(0) > 0).numpy()
-		cv2.imwrite(os.path.join(save_dir, 'vis_lines_label.png'), (vis_all_ori * 255).astype(np.uint8))
-
-		# plt.figure()
-		# plt.imshow(vis_all_ori)
-
-		vis_img = ori_gt.copy()
-		vis_img[weights < 1.0] = 2.0
-		cv2.imwrite(os.path.join(save_dir, 'vis_gt.png'), (vis_img * 127.0).astype(np.uint8))
-
-		# plt.figure()
-		# plt.imshow(vis_img)
-
-		rotated_filter = self.get_weight(max_idx)[0,0].numpy()
-		fig = plt.figure()
-		plt.imshow(rotated_filter)
-		fig.savefig(os.path.join(save_dir, 'idx.png'))
-
-		rotated_filter = self.get_weight(max_idx+1)[0,0].numpy()
-		fig = plt.figure()
-		plt.imshow(rotated_filter)
-		fig.savefig(os.path.join(save_dir, 'idx2.png'))
 
 
 class OrientedConv2d(_ConvNd):
